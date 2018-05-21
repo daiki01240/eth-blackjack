1ba1437e0770b3dddc140bf4fbafd218505957d2
root <root@tk2-238-28766.vs.sakura.ne.jp>
2018-05-21 23:44:28 +0900

    [update]update view

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@
 Auth::routes();
 
 Route::group(['middleware'=>'guest'],function(){
-	Route::get('/',['as'=>'dashboard','uses'=>'DashboardController@index']);
-	Route::get('/typography',['as'=>'typography','uses'=>'TypographyController@index']);
+	Route::get('/dashboard',['as'=>'dashboard','uses'=>'DashboardController@index']);
+	Route::get('/',['as'=>'typography','uses'=>'TypographyController@index']);
 	Route::get('/helper',['as'=>'helper','uses'=>'HelperController@index']);
 	Route::get('/widget',['as'=>'widget','uses'=>'WidgetController@index']);
 	Route::get('/table',['as'=>'table','uses'=>'TableController@index']);
